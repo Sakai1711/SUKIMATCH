7751a1e8f38a6d51bcbfef9efa4ba8c78e9acedf
toy101 <ttsuneda@csl.ec.t.kanazawa-u.ac.jp>
2020-09-17 16:28:59 +0900

    Combine functions of editing and deleting user information by tsuneda

--- back/api/userApi/test.py
+++ back/api/userApi/test.py
@@ -2,11 +2,12 @@ import requests
 
 def test_api(api_url):
     request_param = {
-        "password": "hoge"
+        "password": "hoge",
+        "is_delete" : True
     }
     res = requests.post(api_url, json=request_param)
     print(res.json())
 
 if __name__ == "__main__":
-    api_url = "http://localhost:5000/user"
+    api_url = "http://localhost:5000/user/:id"
     test_api(api_url)
