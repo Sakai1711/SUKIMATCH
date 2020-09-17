46ae30f6795ed848bcfa406450a8a528db4b2173
飯盛正慶 <iimorimasamichi@masamichi.local>
2020-09-17 14:23:45 +0900

    create home page

---
+++ front/sukimatch-front/src/components/Home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
+
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    maxWidth: 500,
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  },
+  button: {
+    marginRight: 'auto',
+    marginLeft: 'auto'
+  }
+
+});
+
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h2" gutterBottom>
+        SukiMatch
+      </Typography>
+      {/* <Button className={classes.button}></Button> */}
+      <Link to='/sign_in'>始める</Link>
+    </div>
+  );
+}
