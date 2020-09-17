efe96ce09d03121d8f994b393b6b317c043d9c7b
toy101 <ttsuneda@csl.ec.t.kanazawa-u.ac.jp>
2020-09-17 15:13:14 +0900

    add hash func and update by tsuneda.

--- back/api/userApi/test.py
+++ back/api/userApi/test.py
@@ -4,9 +4,9 @@ def test_api(api_url):
     request_param = {
         'param': 0
     }
-    res = requests.delete(api_url, json=request_param)
-    print(res)
+    res = requests.post(api_url, json=request_param)
+    print(res.json())
 
 if __name__ == "__main__":
-    api_url = "http://localhost:5000/user/:id"
+    api_url = "http://localhost:5000/user"
     test_api(api_url)
