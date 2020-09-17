0978a13feff9e4c32478be7401b791957fec742f
酒井敦也 <sakai.atsuya.g@gmail.com>
2020-09-17 15:15:24 +0900

    [update - tag_api] add delete tag function

--- back/api/tag_api.py
+++ back/api/tag_api.py
@@ -29,5 +29,19 @@ def get_tags():
     #########
     return jsonify({ 'tags': tags }), 200
 
+@app.route("/tag/delete", methods=["POST"])
+def delete_tag():
+    access_token = request.headers.get("access_token")
+    given_json = request.json
+    tag_name = given_json['tag_id']
+    exist_flag = 1
+    #########
+    ## DB処理
+    #########
+    if exist_flag == 1:
+        return jsonify({}), 200
+    else:
+        return jsonify({}), 404
+
 if __name__ == "__main__":
     app.run(debug=False, host='0.0.0.0', port=5000)
