1e5aae37e358faa2dfc93ceef9da937438dfb442
酒井敦也 <sakai.atsuya.g@gmail.com>
2020-09-18 10:19:16 +0900

    [update - chatroom] change chatroom_id type

--- back/api/chatroom_api.py
+++ back/api/chatroom_api.py
@@ -2,9 +2,8 @@ from flask import Flask, request, jsonify
 import conf
 app = conf.app
 
-@app.route("/chatrooms/search/<int:chatroom_id>", methods=["GET"])
+@app.route("/chatrooms/search/<chatroom_id>", methods=["GET"])
 def search_chatroom_id(chatroom_id):
-    print(chatroom_id)
     access_token = request.headers.get("access_token")
     #########
     ## DB処理
@@ -19,7 +18,7 @@ def create_chatroom():
     access_token = request.headers.get("access_token")
     given_json = request.json
     tag_id = given_json['tag_id']
-    chatroom_id = 0
+    chatroom_id = 'aaa'
     #########
     ## DB処理
     #########
