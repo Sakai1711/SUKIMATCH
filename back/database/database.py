70eed54c4689cfe8d9132849877eb2fbcf71f679
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-09-18 13:08:33 +0900

    fix path and delete some import by nagai

--- back/database/database.py
+++ back/database/database.py
@@ -2,7 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-cred = credentials.Certificate("RAKUTEN/sukimatch/TeamA/back/database/sukimatch-21753-firebase-adminsdk-pbpyr-b48f70c7fe.json")
+cred = credentials.Certificate("back/database/sukimatch-21753-firebase-adminsdk-pbpyr-b48f70c7fe.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
