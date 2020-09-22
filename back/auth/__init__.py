0437836dd6f71ffcbd67bd592601380bb7f7cb84
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-09-22 12:30:06 +0900

    first deploy

--- back/auth/__init__.py
+++ back/auth/__init__.py
@@ -1,8 +1,17 @@
 import pyrebase
 import json
+import os
 
-conf = json.load(open("back/conf.json", 'r'))
-firebase = pyrebase.initialize_app(conf)
+API_KEY = os.environ['API_KEY']
+PRJ_ID = os.environ['PRJ_ID']
+config = {
+  "apiKey": API_KEY,
+  "authDomain": PRJ_ID + ".firebaseapp.com",
+  "databaseURL": "https://" + PRJ_ID + ".firebaseio.com/",
+  "storageBucket": PRJ_ID + ".appspot.com"
+}
+#conf = json.load(open("back/conf.json", 'r'))
+firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
 from .auth import signup, signin, verify
--- back/database/__init__.py
+++ back/database/__init__.py
@@ -1,8 +1,29 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
-cred = credentials.Certificate("back/sukimatch-21753-firebase-adminsdk-pbpyr-b48f70c7fe.json")
+PRIVATE_KEY_ID = os.environ["PRIVATE_KEY_ID"]
+PRIVATE_KEY = os.environ["PRIVATE_KEY"]
+CLIENT_EMAIL = os.environ["CLIENT_EMAIL"]
+CLIEND_ID = os.environ["CLIENT_ID"]
+AUTH_URI = os.environ["AUTH_URI"]
+TOKEN_URI = os.environ["TOKEN_URI"]
+AUTH_PROVIDER = os.environ["AUTH_PROVIDER"]
+CLIENT_URL = os.environ["CLIENT_URL"]
+#cred = credentials.Certificate("back/sukimatch-21753-firebase-adminsdk-pbpyr-b48f70c7fe.json")
+cred = {
+  "type": "service_account",
+  "project_id": "sukimatch-21753",
+  "private_key_id": PRIVATE_KEY_ID,
+  "private_key": PRIVATE_KEY,
+  "client_email": CLIENT_EMAIL,
+  "client_id": CLIEND_ID,
+  "auth_uri": AUTH_URI,
+  "token_uri": TOKEN_URI,
+  "auth_provider_x509_cert_url": AUTH_PROVIDER,
+  "client_x509_cert_url": CLIENT_URL
+}
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
