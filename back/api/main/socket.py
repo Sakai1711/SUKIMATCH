12fa0a186d9e8f128244f0d6f5597a4ae912f131
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-09-23 00:31:27 +0900

    fix socket

--- back/api/main/socket.py
+++ back/api/main/socket.py
@@ -9,34 +9,30 @@ from database.chatroom import delete_chatroom
 from auth.auth import verify
 
 class MyNamespace(Namespace):
-    def on_connect_req(self, data): # connect
-        data = data.json
+    def on_connect(self, data): # connect
         access_token = data['access_token']
         chatroom_id = data['chatroom_id']
         if verify(access_token) != "":
             join_room(chatroom_id)
-            result = jsonify({'status': 'ok'})
-            emit('connect_res', result)
+            emit('connect_res', {'status': 'ok'})
 
         # TODO: write else response
-
+        # 
     def on_send_message_req(self, data): # send message
-        data = data.json
         access_token = data['access_token']
         chatroom_id = data['chatroom_id']
         content = data['content']
         user_id = verify(access_token)
         if user_id != "":
             username, _, _ = load_mypage(user_id)
-            result = jsonify({'username': username, 'content': content})
+            result = {'username': username, 'content': content}
             emit('send_message_res', result, room_id=chatroom_id)
 
-    def on_disconnect_req(self, data): # disconnect
-        data = data.json
+    def on_disconnect(self, data): # disconnect
         access_token = data['access_token']
         chatroom_id = data['chatroom_id']
         user_id = verify(access_token)
         if user_id != "":
             delete_chatroom(chatroom_id)
 
-socketio.on_namespace(MyNamespace('/chatrooms'))
+socketio.on_namespace(MyNamespace('/chat'))
