1f88be4ea81c95b32464ca076494afdd1fa50752
Enrai22 <mihane.watanabe22@gmail.com>
2020-09-23 09:21:27 +0900

    front_develop/loginpage validation追加

--- front/sukimatch-front/src/components/Login.js
+++ front/sukimatch-front/src/components/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -15,104 +15,137 @@ import FormControl from '@material-ui/core/FormControl';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    maxWidth: 500,
-    marginRight: 'auto',
-    marginLeft: 'auto'
-  },
-  Button: {
-    marginRight: 'auto',
-    marginLeft: 'auto'
+export default class Login extends Component{
+  constructor(props){
+      super(props);
+      this.state={
+          email:'',
+          password:'',
+          isSubmitted: false,
+          hasEmailError: false,
+          hasPassError: false,
+          showPassword: false,
+          canSubmit: false,
+        };
   }
-});
 
-export default function Login() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    showPassword: false,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+  handleChange = (prop) => (event) => {
+    this.setState({[prop]: event.target.value });
   };
 
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
+  handleClickShowPassword = () => {
+    this.setState({showPassword: !this.state.showPassword });
   };
 
-  const handleMouseDownPassword = (event) => {
+  handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
+  handleClickisSubmitted = () => {
+    this.setState({isSubmitted: !this.state.isSubmitted });
+  }
 
-  return (
-    <div className={classes.root}>
+  isEmptyEmail = () => {
+    if(this.state.username === ''){
+      this.setState({hasEmailError: true});
+    }else{
+      this.setState({hasEmailError: false});
+    }
+  }
+
+  isEmptyPass = () => {
+    if(this.state.username === ''){
+      this.setState({hasPassError: true});
+    }else{
+      this.setState({hasPassError: false});
+    }
+  }
+
+
+  render(){
+    return (
+      <div className='loginpage'>
+          <br /><br />
+        <Typography variant="h2" gutterBottom>
+          Login
+        </Typography>
         <br /><br />
-      <Typography variant="h2" gutterBottom>
-        Login
-      </Typography>
-      <br /><br />
-      <Box textAlign="center">
-        <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="standard-basic" label="Name" />
-        <br /><br />
-
-        <FormControl className="passform">
-          <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
-          <Input
-            id="standard-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
+        <Box textAlign="center">
+          <form className='form' noValidate autoComplete="off">
+          <TextField 
+            id="standard-basic" 
+            label="Email"
+            onChange={this.handleChange('email')}
+            onBlur={this.isEmptyEmail}
           />
-        </FormControl>
+          {this.state.hasEmailError ?
+            <FormHelperText id="component-error-text">
+              Email is required.
+            </FormHelperText> : <br />}
+          <br />
 
-            <br /><br />
-        <FormControl error className="passform-error" color="secondary">
-          <InputLabel htmlFor="component-error">Password</InputLabel>
-          <Input
-            id="standard-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.password}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-          />
-          <FormHelperText id="component-error-text">Error: Passwords do not match.</FormHelperText>
-        </FormControl>
+          <FormControl className="passform">
+            <InputLabel htmlFor="standard-adornment-password">Password</InputLabel>
+            <Input
+              id="standard-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              onBlur={this.isEmptyPass}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+            {this.state.hasPassError ?
+            <FormHelperText id="component-error-text">
+              Password is required.
+            </FormHelperText> : <br />}
+          </FormControl>
 
-    </form>
-    <br /><br /><br />
-    <Button variant="contained" color="primary" href="/user">
-        Login
-    </Button>
-    {/* space */}　{/* space */}
-    <Button variant="contained" color="primary" href="/signup">
-        Signup
-    </Button>
-    </Box>
-    </div>
-  );
+              <br /><br />
+          <FormControl error className="passform-error" color="secondary">
+            <InputLabel htmlFor="component-error">Password</InputLabel>
+            <Input
+              id="standard-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+            <FormHelperText id="component-error-text">Error: Passwords do not match.</FormHelperText>
+          </FormControl>
+
+      </form>
+      <br /><br /><br />
+      <Button className='button' variant="contained" color="primary" href="/user"
+              onClick={this.handleClickisSubmitted}>
+          Login
+      </Button>
+      {/* space */}　{/* space */}
+      <Button className='button' variant="contained" color="primary" href="/signup">
+          Signup
+      </Button>
+      <br /><br /><br />
+      </Box>
+      </div>
+    );
+  }
 }
--- front/sukimatch-front/src/components/Signup.js
+++ front/sukimatch-front/src/components/Signup.js
@@ -1,12 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { Box } from '@material-ui/core';
 import IconButton from '@material-ui/core/IconButton';
-import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import InputAdornment from '@material-ui/core/InputAdornment';
@@ -14,165 +12,262 @@ import FormHelperText from '@material-ui/core/FormHelperText';
 import FormControl from '@material-ui/core/FormControl';
 import Visibility from '@material-ui/icons/Visibility';
 import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Checkbox from '@material-ui/core/Checkbox';
 
+export default class Signup extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state={
+      username:'',
+      email:'',
+      password:'',
+      confirmpass:'',
+      conPassError: false,
+      showPassword: false,
+      hasNameError: false,
+      hasEmailError: false,
+      hasPassError: false,
+      createPasswordError: false,
+      canSubmit: false,
+      isSubmitted: false,
+      enterLastCheck: false,
+      };
+}
 
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    maxWidth: 500,
-    marginRight: 'auto',
-    marginLeft: 'auto'
-  },
-  Button: {
-    marginRight: 'auto',
-    marginLeft: 'auto'
-  }
-
-});
-
-export default function Signup() {
-  const classes = useStyles();
-  const [values, setValues] = React.useState({
-    showPassword: false,
-  });
-
-  const handleChange = (prop) => (event) => {
-    setValues({ ...values, [prop]: event.target.value });
+  handleChange = (prop) => (event) => {
+    this.setState({[prop]: event.target.value });
   };
 
-  const handleClickShowPassword = () => {
-    setValues({ ...values, showPassword: !values.showPassword });
+  handleClickShowPassword = () => {
+    this.setState({showPassword: !this.state.showPassword });
   };
 
-  const handleMouseDownPassword = (event) => {
+  handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
 
+  matchPassword = () => {
+    if (this.state.password !== this.state.confirmpass){
+      this.setState({conPassError: true });
+    }else{
+      this.setState({conPassError: false });
+    }
+  };
 
-  return (
-    <div className={classes.root}>
-        <br /><br />
-      <Typography variant="h2" gutterBottom>
-        Signup
-      </Typography>
-      <br /><br />
-      <Box textAlign="center">
-        <form className={classes.root} noValidate autoComplete="off">
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <br /><br />
-        <TextField id="outlined-basic" label="Email" variant="outlined" />
-        <br /><br />
+  isEmptyName = () => {
+    if(this.state.username === ''){
+      this.setState({hasNameError: true});
+    }else{
+      this.setState({hasNameError: false});
+    }
+  }
 
-    {/* ---通常パスワード--- */}
+  isEmptyEmail = () => {
+    if(this.state.username === ''){
+      this.setState({hasEmailError: true});
+    }else{
+      this.setState({hasEmailError: false});
+    }
+  }
 
-        <FormControl className="passform" variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.normalpass}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
+  isEmptyPass = () => {
+    if(this.state.username === ''){
+      this.setState({hasPassError: true});
+    }else{
+      this.setState({hasPassError: false});
+    }
+  }
+
+  confirmSubmit = () => {
+    if (this.state.hasEmailError || this.state.hasNameError || this.state.hasPassError || this.state.isSubmitted || this.state.conPassError || this.state.lastCheck){
+      this.setState({canSubmit: false});
+    }else{
+      this.setState({canSubmit: true});
+    }
+  }
+
+  lastCheck = () => {
+   // this.isEmptyName;
+   // this.isEmptyEmail;
+   //this.isEmptyPass;
+   // this.matchPassword;
+    this.setState({enterLastCheck: !this.state.enterLastCheck });
+  };
+
+  render(){
+    return (
+      <div className='signup'>
+          <br /><br />
+        <Typography className='title' variant="h2" gutterBottom>
+          Signup
+        </Typography>
+        <br />
+        <Box textAlign="center">
+          <form className='form' noValidate autoComplete="off">
+          <TextField
+            required
+            id="outlined-basic" 
+            label="Name"
+            variant="outlined" 
+            onChange={this.handleChange('username')}
+            onBlur={this.isEmptyName}
+            />
+            {this.state.hasNameError ?
+            <FormHelperText id="component-error-text">
+              Name is required.
+            </FormHelperText> : <br />}
+          <br />
+          <TextField
+            id="outlined-basic"
+            type="email"
+            required
+            label="Email"
+            variant="outlined"
+            onChange={this.handleChange('email')}
+            onBlur={this.isEmptyEmail}
+            />
+            {this.state.hasEmailError ?
+            <FormHelperText id="component-error-text">
+              Email is required.
+            </FormHelperText> : <br />}
+            <br />
+
+      {/* ---通常パスワード--- */}
+
+          <FormControl className="passform" variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              onBlur={this.isEmptyPass}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+            {this.state.hasPassError ?
+            <FormHelperText id="component-error-text">
+              Password is required.
+            </FormHelperText> : <br />}
+          </FormControl>
+          <br />
+
+      {/* ---通常パスワードのエラー--- */}
+      {/*
+          <FormControl error className="passform" variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              required
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+            <FormHelperText id="component-error-text">A password is invalid.</FormHelperText>
+          </FormControl>
+          <br /><br />
+            */}
+      {/* ---確認用パスワードのエラー--- */}
+      {this.state.conPassError ?
+          <FormControl error className="passform" variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              required
+              value={this.state.confirmpass}
+              onChange={this.handleChange('confirmpass')}
+              onBlur={this.matchPassword}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+            <FormHelperText id="component-error-text">
+              Confirm password do not match.
+            </FormHelperText>
+          </FormControl>:
+          <FormControl className="passform" variant="outlined">
+            <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={this.state.showPassword ? 'text' : 'password'}
+              required
+              value={this.state.confirmpass}
+              onChange={this.handleChange('confirmpass')}
+              onBlur={this.matchPassword}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={this.handleClickShowPassword}
+                    onMouseDown={this.handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              labelWidth={70}
+            />
+          </FormControl>
             }
-            labelWidth={70}
-          />
-        </FormControl>
+        </form>
         <br /><br />
-
-    {/* ---通常パスワードのエラー--- */}
-
-        <FormControl error className="passform" variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.normalpass}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={70}
+        <Checkbox
+            onChange={this.lastCheck}
+            onClick={this.confirmSubmit}
+            onBlur={this.confirmSubmit}
+            name="checkedB"
+            color="primary"
           />
-          <FormHelperText id="component-error-text">A password is invalid.</FormHelperText>
-        </FormControl>
-        <br /><br />
-
-    {/* ---確認用パスワード--- */}
-
-        <FormControl className="passform" variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.confirmpass}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={70}
-          />
-        </FormControl>
-        <br /><br />
-
-    {/* ---確認用パスワードのエラー--- */}
-
-        <FormControl error className="passform" variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Confirm Password</InputLabel>
-          <OutlinedInput
-            id="outlined-adornment-password"
-            type={values.showPassword ? 'text' : 'password'}
-            value={values.confirmpass}
-            onChange={handleChange('password')}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
-                >
-                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            labelWidth={70}
-          />
-          <FormHelperText id="component-error-text">Confirm password do not match.</FormHelperText>
-        </FormControl>
-      </form>
-      <br /><br /><br />
-        <Button variant="contained" color="primary" href="/user">
+          Have you completed the input?
+          <br />
+          {this.state.canSubmit ?
+          <Button variant="contained" color="primary" href="/user" onChange={this.handleChange('isSubmitted')}>
+              Sign up
+          </Button>
+          : 
+          <Button variant="contained" disabled>
             Sign up
-        </Button>
-     </Box>
-    </div>
-  );
+          </Button>}
+          <br /><br /><br />
+      </Box>
+      </div>
+    );
+  }
 }
