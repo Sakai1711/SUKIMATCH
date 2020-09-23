6f8f2012a76a352a9c073c58fb5712ba9d2a4716
永井琉生 <ryusei@nagairyuseinoMacBook-puro.local>
2020-09-23 19:50:34 +0900

    1つのファイルに

--- back/app.py
+++ back/app.py
@@ -258,7 +258,7 @@ class MyNamespace(Namespace):
     def on_ping_ping(self, data):
         print("ping")
         time.sleep(3)
-        emit('pong_pong')
+        socketio.emit('pong_pong')
 
     def on_connect_req(self, data): # connect
         print("connect_req")
@@ -267,9 +267,9 @@ class MyNamespace(Namespace):
         print("chatroom_id: ", chatroom_id)
         if verify(access_token) != "":
             #join_room(chatroom_id)
-            emit('connect_res', {'status': 'ok'})
+            socketio.emit('connect_res', {'status': 'ok'})
         else:
-            emit('connect_res', {'status': 'incorrect access token'})
+            socketio.emit('connect_res', {'status': 'incorrect access token'})
 
     def on_send_message_req(self, data): # send message
         print("send_message_req")
@@ -282,9 +282,9 @@ class MyNamespace(Namespace):
         if user_id != "":
             username, _ = load_mypage(user_id)
             result = {'username': username, 'content': content, 'access_token': access_token}
-            emit('send_message_res', result, room_id=chatroom_id)
+            socketio.emit('send_message_res', result, room_id=chatroom_id)
         else:
-            emit('send_message_res', {'status': 'incorrect access token'})
+            socktio.emit('send_message_res', {'status': 'incorrect access token'})
 
     def on_disconnect_req(self, data): # disconnect
         print("disconnect")
