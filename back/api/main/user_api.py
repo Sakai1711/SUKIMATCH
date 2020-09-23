2b8f3d6133c1b30d33eb66838acf3ce7aa74fd98
toy101 <ttsuneda@csl.ec.t.kanazawa-u.ac.jp>
2020-09-23 09:52:12 +0900

    Use database/tag.py to handle tags and fix typo.

--- back/api/main/user_api.py
+++ back/api/main/user_api.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('../')
 
 from database.user import add_new_user, load_mypage, update_data, delete_data
+from database.tag import insert_tag, get_tags, delete_tag
 from auth.auth_user import signup, signin, verify, refresh_token
 from auth.update import update_user
 from auth.delete import delete_user
@@ -66,6 +67,7 @@ def load_user_page():
         return jsonify({}), 401
 
     username, email = load_mypage(user_id)
+    tags = get_tags(user_id)
 
     # provisional
     # username = "hoge"
@@ -83,13 +85,14 @@ def load_user_page():
     responsed_json = {
         "username": username,
         "email": email,
+        "tag": tags
     }
 
     return jsonify(responsed_json), 200
 
 
 @app.route("/user/edit", methods=["POST"])
-def operate_user_page():
+def edit_user_page():
     access_token = request.headers.get("access_token")
     # Convert token to ID
     user_id = verify(access_token)
@@ -98,6 +101,18 @@ def operate_user_page():
 
     given_json = request.json
     given_json["user_id"] = user_id
+
+    # tags
+    past_tags = transform_tags_dict(get_tags(user_id))
+    current_tags = transform_tags_dict(given_json["tag"])
+    # New Tag
+    for t_id, t_name in zip(current_tags["ids"], current_tags["names"]):
+        if not t_id in past_tags["ids"]:
+            insert_tag(user_id, t_name)
+    # Delete Tag
+    for t_id, t_name in zip(past_tags["ids"], past_tags["names"]):
+        if not t_id in current_tags["ids"]:
+            delete_tag(user_id, t_name)
 
     # update Auth data
     user = update_user(given_json["user_id"], given_json["email"], given_json["password"])
@@ -112,10 +127,29 @@ def operate_user_page():
     new_token = refresh_token(given_json["token"])
 
     responsed_json = {
-        "user_id": new_token
+        "token": new_token
     }
 
     return jsonify(responsed_json), 200
+
+def transform_tags_dict(tags):
+    """
+    tag = [ {"id": 0,
+            "tag_name": "test1"},
+            {"id": 1,
+            "tag_name": "test2"}]
+    ->
+    tag = {"ids" : [0, 1],
+           "names" : ["test1", "test2"]}
+    """
+    transformed = {"ids":[], "tag_name":[]}
+
+    for t in tags:
+        transformed["ids"].append(t["id"])
+        transformed["tag_name"].append(t["tag_name"])
+
+    return transformed
+
 
 @app.route("/user/delete", methods=["POST"])
 def delete_account():
--- back/database/model.py
+++ back/database/model.py
@@ -34,15 +34,20 @@ class User(object):
         )
 
 class Tag(object):
-    def __init__(self, tag_name):
+    def __init__(self, user_id, tag_name):
         self.tag_name = tag_name
+        self.user_id = user_id
 
     def to_dict(self):
-        return {'tag_name': self.tag_name}
+        return {
+            'user_id': self.user_id,
+            'tag_name': self.tag_name
+        }
 
     def __repr__(self):
         return (
             f'Tag(\
+                user_id={self.user_id}, \
                 tag_name={self.tag_name}\
             )'
         )
