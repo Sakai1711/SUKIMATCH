e58b5322fc9f9937468af744eed77107f049da28
白石誠 <makotinator.15@gmail.com>
2020-09-25 09:08:00 +0900

    minor change

--- front/sukimatch-front/src/components/ChatPage/ChatHeader.js
+++ front/sukimatch-front/src/components/ChatPage/ChatHeader.js
@@ -5,7 +5,7 @@ class ChatHeader extends Component {
   constructor() {
     super();
     this.state = {
-      time: 65,
+      time: 10,
     };
     this.countDown = this.countDown.bind(this);
   }
